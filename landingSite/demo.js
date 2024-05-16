08931d0d1373d4dceeedc90288b6e35473600a1e
Mark <41304553+MarkV2323@users.noreply.github.com>
2024-05-15 22:42:37 -0600

    Started JS part of tutorial

---
+++ landingSite/demo.js
@@ -0,0 +1,17 @@
+console.log("Hello, World!")
+console.log("I am logging more information to the screen from JS")
+
+let a = 10
+let b = 20
+console.log(a)
+console.log("" + a + " + " + b);
+
+const MAX = 57
+console.log(MAX)
+
+let stringOne = "This is a string, isn't it great?"
+console.log(stringOne)
+
+let cocat = `${stringOne} and the MAX is ${MAX}`
+console.log(cocat)
+
