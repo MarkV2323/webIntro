8c25d75e654152434796551942f6381405281725
Mark <41304553+MarkV2323@users.noreply.github.com>
2024-05-18 22:15:50 -0600

    Created time function

--- clockSite/clock.js
+++ clockSite/clock.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+// Get browser time in mil format
+function getTimeString () {
+    const d = new Date();
+    const hour = d.getHours();
+    const min = d.getMinutes();
+    const sec = d.getSeconds();
+    const timeString = `${hour}:${min}:${sec}`
+    return timeString;
+}
+
+const timeS = getTimeString();
+console.log(timeS);
